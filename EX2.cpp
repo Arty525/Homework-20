ee9666e625730e0c9cde013139de942baef26c7c
Andrey <89473462+Arty525@users.noreply.github.com>
2022-03-15 15:03:49 +0300

    Add files via upload

---
+++ EX2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <ctime>
+#include <fstream>
+
+using namespace std;
+
+int main() {
+	int width;
+	int height;
+
+	srand(time(nullptr));
+
+	cout << "Enter width and height: ";
+	cin >> width >> height;
+
+	ofstream picture("D:\\RTY\\GITHUB\\Repositories\\homework-20\\EX2\\pic.txt");
+	for (int i = 0; i < height; ++i) {
+		for (int j = 0; j < width; ++j) {
+			picture << rand() % 2;
+		}
+		picture << endl;
+	}
+	picture.close();
+
+	return 0;
+}
