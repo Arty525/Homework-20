aa40222c0360581b004d2b789dcfd20e96b01111
Andrey <89473462+Arty525@users.noreply.github.com>
2022-03-19 14:01:45 +0300

    Add files via upload

---
+++ EX3.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+	std::ifstream fishing;
+	std::string fish;
+	std::string caught;
+	int count = 0;
+
+	std::cout << "Choose your fish: ";
+	std::cin >> fish;
+
+	fishing.open("D:\\RTY\\GITHUB\\Repositories\\homework-20\\EX3\\river.txt");
+	while (getline(fishing, caught)) {
+		if (caught == fish) {
+			std::ofstream basket("D:\\RTY\\GITHUB\\Repositories\\homework-20\\EX3\\basket.txt", std::ios::app);
+			basket << caught << std::endl;
+			++count;
+		}
+	}
+
+	std::cout << "You are caught " << count << " fishes." << std::endl;
+}
---
+++ EX4.cpp
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <ctime>
+
+std::ifstream readBank;
+std::ofstream writeBank;
+
+void add(std::vector<int> bills) {
+
+}
+void withdrawal(int cash) {
+	
+}
+
+int main() {
+	std::vector<int> bills = { 100, 200, 500, 1000, 2000, 5000 };
+	std::string operation;
+	int cash;
+
+	srand(time(nullptr));
+
+	std::cout << "What operation do you want to perform? withdraw/replenish: ";
+	std::cin >> operation;
+
+	if (operation == "+") add(bills);
+
+	if (operation == "-") {
+		std::cout << "Enter summ: ";
+		std::cin >> cash;
+
+		withdrawal(cash);
+	}
+	if (operation == "read") {
+		int number;
+		std::ifstream file("D:\\RTY\\GITHUB\\Repositories\\homework-20\\EX4\\bank.bin", std::ios::binary);
+		while (!file.eof()) {
+			file.read((char*)&number, sizeof(number));
+			std::cout << number << std::endl;
+		}
+		file.close();
+	}
+	return main();
+}
