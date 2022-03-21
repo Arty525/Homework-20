90a651b02c88f5f0acfbfeef0a7c6252f949c537
Andrey <89473462+Arty525@users.noreply.github.com>
2022-03-21 16:48:46 +0300

    Add files via upload

--- EX4.cpp
+++ EX4.cpp
@@ -7,10 +7,58 @@ std::ifstream readBank;
 std::ofstream writeBank;
 
 void add(std::vector<int> bills) {
+	int count = 0;
+	int bill;
+
+	readBank.open("D:\\RTY\\GITHUB\\Repositories\\homework-20\\EX4\\bank.bin", std::ios::binary);
+	while (!readBank.eof()) {
+		readBank.read((char*)&bill, sizeof(bill));
+		count += readBank.gcount();
+	}
+	readBank.close();
+
+	if (count < 4000) {
+		writeBank.open("D:\\RTY\\GITHUB\\Repositories\\homework-20\\EX4\\bank.bin", std::ios::app);
+		while (count < 4000) {
+			int random = bills[rand() % 6];
+			writeBank.write((char*)&random, sizeof(random));
+			count += sizeof(random);
+		}
+		writeBank.close();
+	}
+
 
 }
-void withdrawal(int cash) {
-	
+
+void out(int cash) {
+	int bill;
+	int zero = 0;
+	int summ = 0;
+
+	readBank.open("D:\\RTY\\GITHUB\\Repositories\\homework-20\\EX4\\bank.bin", std::ios::binary);
+	writeBank.open("D:\\RTY\\GITHUB\\Repositories\\homework-20\\EX4\\buffer.bin", std::ios::binary);
+	while (!readBank.eof()) {
+		readBank.read((char*)&bill, sizeof(bill));
+		if ((cash - summ - bill >= 0 || (cash - summ) % bill == 0) && summ != cash) {
+			writeBank.write((char*)&zero, 0);
+			summ += bill;
+
+		}
+		else {
+			writeBank.write((char*)&bill, sizeof(bill));
+		}
+	}
+	readBank.close();
+	writeBank.close();
+
+	readBank.open("D:\\RTY\\GITHUB\\Repositories\\homework-20\\EX4\\buffer.bin", std::ios::binary);
+	writeBank.open("D:\\RTY\\GITHUB\\Repositories\\homework-20\\EX4\\bank.bin", std::ios::binary);
+	while (!readBank.eof() - 1) {
+		readBank.read((char*)&bill, sizeof(bill));
+		writeBank.write((char*)&bill, sizeof(bill));
+	}
+	readBank.close();
+	writeBank.close();
 }
 
 int main() {
@@ -29,16 +77,7 @@ int main() {
 		std::cout << "Enter summ: ";
 		std::cin >> cash;
 
-		withdrawal(cash);
+		out(cash);
 	}
-	if (operation == "read") {
-		int number;
-		std::ifstream file("D:\\RTY\\GITHUB\\Repositories\\homework-20\\EX4\\bank.bin", std::ios::binary);
-		while (!file.eof()) {
-			file.read((char*)&number, sizeof(number));
-			std::cout << number << std::endl;
-		}
-		file.close();
-	}
-	return main();
+	return 0;
 }
